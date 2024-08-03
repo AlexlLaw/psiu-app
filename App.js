28e12d0559e0496d14ec86c1b571fccffb343d44
Eduardo <ads.eduardo.silva@gmail.com>
2024-08-03 18:26:36 -0300

    ajuste do botão de voltar

--- App.js
+++ App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Onboarding } from './Onboarding';
 import { WebView } from 'react-native-webview';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, BackHandler, View, Text } from 'react-native';
 import 'react-native-gesture-handler';
 
 const Stack = createStackNavigator();
 
 function WebViewScreen({ route }) {
   const { url } = route.params;
-  return <WebView source={{ uri: url }} style={{ flex: 1 }} />;
-}
+  const webViewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const onBackPress = () => {
+      if (canGoBack) {
+        webViewRef.current.goBack();
+        return true;
+      }
+      BackHandler.exitApp();
+      return false;
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    };
+  }, [canGoBack]);
+
+  return (
+    <WebView
+      ref={webViewRef}
+      source={{ uri: url }}
+      style={{ flex: 1 }}
+      onNavigationStateChange={navState => setCanGoBack(navState.canGoBack)}
+    />
+  );
+} 
 
 export default function App() {
   return (
